701d5c426be157b68d464fcfbf30b49dbf1f51e9
TalMoshel1 <89250740+TalMoshel1@users.noreply.github.com>
2023-02-20 23:04:03 +0200

    configg

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import './index.css'
 import { Route, Routes, HashRouter } from "react-router-dom";
 import  GameProvider  from './store/gameContext';
 import Home from './Home'
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: [
+        /^node:.*/,
         'react-is'
       ]
     }
