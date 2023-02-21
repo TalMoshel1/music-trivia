c728c897728b25e2f7e1a1bc546f2affc7528908
TalMoshel1 <89250740+TalMoshel1@users.noreply.github.com>
2023-02-21 20:34:21 +0200

    build4

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-        output: {
-            globals: {
-              'react-is': 'ReactIs'
-            }
-          },
-      external: [
-        /^node:.*/,
-        'react-is'
-      ]
-    }
-  }
+//   build: {
+//     rollupOptions: {
+//         output: {
+//             globals: {
+//               'react-is': 'ReactIs'
+//             }
+//           },
+//       external: [
+//         /^node:.*/,
+//         'react-is'
+//       ]
+//     }
+//   }
 })
